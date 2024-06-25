71220d3ea50d6e3ad1cae2f86324b39f9320a9a8
NicoleOkamoto <nicoleokamoto@icloud.com>
2024-06-25 11:01:07 -0500

    0.0.4

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,68 @@
-import React from "react";
-import './Button.css';
+// import React from "react";
+// import './Button.css';
 
-interface ButtonProps {
-  label: string;
-}
+// interface ButtonProps {
+//   label: string;
+// }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+// const Button = (props: ButtonProps) => {
+//   return <button>{props.label}</button>;
+// };
+
+// export default Button;
+// components/button/button.tsx
+import React, { MouseEventHandler } from "react";
+import styled from "styled-components";
+
+export type ButtonProps = {
+  text?: string;
+  primary?: boolean;
+  disabled?: boolean;
+  size?: "small" | "medium" | "large";
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  label?: string;
+};
+
+const StyledButton = styled.button<ButtonProps>`
+  border: 0;
+  line-height: 1;
+  font-size: 15px;
+  cursor: pointer;
+  font-weight: 700;
+  font-weight: bold;
+  border-radius: 10px;
+  display: inline-block;
+  color: ${(props) => (props.primary ? "#fff" : "#000")};
+  background-color: ${(props) => (props.primary ? "#FF5655" : "#f4c4c4")};
+  padding: ${(props) =>
+    props.size === "small"
+      ? "7px 25px 8px"
+      : props.size === "medium"
+      ? "9px 30px 11px"
+      : "14px 30px 16px"};
+`;
+
+const Button: React.FC<ButtonProps> = ({
+  size,
+  primary,
+  disabled,
+  text,
+  onClick,
+  ...props
+}) => {
+  return (
+    <StyledButton
+      type="button"
+      onClick={onClick}
+      primary={primary}
+      disabled={disabled}
+      size={size}
+      {...props}
+    >
+      {text}
+    </StyledButton>
+  );
 };
 
 export default Button;
+
