c71728914eb6446aaee871d0423fc0c28b8ec10b
NicoleOkamoto <nicoleokamoto@icloud.com>
2024-06-25 13:45:40 -0500

    Added Styled-Components

---
+++ src/components/Button/Button.styles.ts
@@ -0,0 +1,22 @@
+// Button.styles.ts
+import styled from 'styled-components';
+import { ButtonProps } from './Button.types';
+
+export const StyledButton = styled.button<ButtonProps>`
+  border: 0;
+  line-height: 1;
+  font-size: 15px;
+  cursor: pointer;
+  font-weight: 700;
+  font-weight: bold;
+  border-radius: 10px;
+  display: inline-block;
+  color: ${(props) => (props.primary ? "#fff" : "#000")};
+  background-color: ${(props) => (props.primary ? "#FF5655" : "#9e141f")};
+  padding: ${(props) =>
+    props.size === "small"
+      ? "7px 25px 8px"
+      : props.size === "medium"
+      ? "9px 30px 11px"
+      : "14px 30px 16px"};
+`;
